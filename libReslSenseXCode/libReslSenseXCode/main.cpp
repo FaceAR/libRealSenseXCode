a7d0055e4a7d17f70e90dad10f603dd366512738
KeeganRen <renqinhu@gmail.com>
2016-07-17 22:11:05 +0800

    first commit

---
+++ libReslSenseXCode/libReslSenseXCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  libReslSenseXCode
+//
+//  Created by Keegan Ren on 7/17/16.
+//  Copyright © 2016 Keegan Ren. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
